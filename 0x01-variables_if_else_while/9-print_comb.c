968ae41cc23ca346814f7af28d34d5406ebfa7ba
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-17 06:35:57 -0700

    adasd

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+    int x = 0 ;
+    int y = 10 ;
+
+    while (x < y)
+     {
+
+ printf("%d,",x);
+ x++;
+     }
+
+
+	return (0);
+}
+
