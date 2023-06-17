a2ab1db38c7833cbd12b860d5b442fbf2d4559e9
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-16 17:30:04 -0700

    asdasd

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stido.h>
+int void ()
+	int x = 1 ;
+	int y = -1 ;
+	int z = 0 ;
+
+	 if(x > y){ 
+		 printf("is positive");
+	 }
+         if else(y < 0){
+		 printf("is negative");
+	 }
+         else(z==0){
+		 printf("is zero");
+	 }
