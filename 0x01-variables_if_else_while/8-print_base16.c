a020cd637f4b2fb16c151085307e374989f0be06
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-17 07:28:39 -0700

    asdasd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+    int x;
+    char latter ;
+
+ for (x = 0; x <= 9 ;x++ )
+ {
+  printf("%d",x);
+ }
+ for(latter ='a';latter <= 'f';latter++)
+ {
+     printf("%c",latter);
+ }
+
+
+
+
+
+
+	return (0);
+
+}
+
