ccb0194e5347c2c5303af1f4b7b2467e413f43d4
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-18 07:08:42 -0700

    sad

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ */
+ int main (void)
+{
+     int letter;
+
+     for(letter = '0'; letter <= '9'; letter++ )
+     {
+            putchar(letter);
+     }
+
+     putchar('\n');
+
+    return(0);
+}
+
