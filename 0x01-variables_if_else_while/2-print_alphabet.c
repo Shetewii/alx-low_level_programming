53deac3123cdbbd6ee3b1c31cbe436108db9de5d
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-18 06:13:13 -0700

    sad

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,13 @@ int main (void){
 char letter;
 
 for(letter = 'a' ; letter <= 'z'; letter++ )
-{
-    if(letter <= 'z'){
 
     putchar(letter);
     putchar('\n');
-}
-}
+
 return(0);
 }
+
+
+
 
