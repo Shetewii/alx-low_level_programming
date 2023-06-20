6bbfd2e1f5d52cbdfe39af1516acf0ac4233b174
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-20 08:30:38 -0700

    sad

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  function that prints 10 times the alphabet
+ *
+ * Return: Always 0(Success).
+ */
+int main(void){
+
+char alph;
+
+for(alph = 'a'; alph <= 'z'; alph++)
+
+    putchar(alph);
+    putchar('\n');
+
+return (0);
+
+}
+
