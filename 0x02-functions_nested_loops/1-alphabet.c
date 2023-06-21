6fe38d599c48f04f4d13eec12a5d1bbcb3845125
Shetewii <mohamed_shetewi@hotmail.com>
2023-06-20 18:11:47 -0700

    sad

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,15 @@
 *
 * Return : Always 0 (Success)
 **/
-void print_alphabet(void){
+void print_alphabet(void)
+{
 
 char  letter;
-
-for(letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
 
 	_putchar(letter);
 
-       _putchar('\n');
+_putchar('\n');
 
 }
